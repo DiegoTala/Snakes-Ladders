6b2a069e5bbefbde142685bd6ed4bd8b61be9883
DiegoTala <74878045+DiegoTala@users.noreply.github.com>
2023-03-22 19:01:04 -0600

    Tentative Final Version of Python Classes

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 from flask import Flask, render_template, request, redirect, url_for, globals
 from flask.views import View
 
-from .models import Board, Dice, Player, Game, Snake, Ladder
+from .models import Board, Dice, Player, Game, Snake, Ladder, Cell
 
 app = Flask(__name__)
 
-# min vals
+#Global Vartiables ot indicate the minimum numbers
 globals.num_users = 2
-globals.board_x = 1
-globals.board_y = 3
+globals.board_x = 10
+globals.board_y = 10
+globals.turns = 10
+globals.difficulty = 'Normal'
+globals.current_turn = 1
+
 
 @app.get('/')
 def home():
@@ -19,12 +23,35 @@ def data():
     globals.num_users = request.form['num_users']
     globals.board_x = request.form['board_width']
     globals.board_y = request.form['board_height']
-    print(request.form['num_users'], request.form['board_width'], request.form['board_height'])
+
+    #Added by Diego T.
+    globals.turns = request.form['num_turns']
+    globals.difficulty = request.form['difficulty']
+
+    print(request.form['num_users'], request.form['board_width'], request.form['board_height'], request.form['num_turns'], request.form['difficulty'])
     return redirect(url_for('board'))
 
 @app.get('/board')
 def board():
-    return f'{globals.num_users} {globals.board_x} {globals.board_y}'
+    return f'{globals.num_users} {globals.board_x} {globals.board_y} {globals.turns} {globals.difficulty}'
+
+@app.post('/game')
+def game():
+    players = [Player(x, 'Name') for x in list(range(1, globals.num_users + 1))]
+    dices = [Dice, Dice]
+    board = Board(globals.board_x, globals.board_y)
+
+    game = Game(players = players, dices = dices, board = board, difficulty = globals.difficulty)
+
+    if globals.current_turn < globals.turns:
+        for player in game.players:
+            increase = game.roll_dices(dices = game.dices)
+            game.change_player_pos_and_score(player, increase)
+            
+        globals.current_turn += 1
+
+    else:
+        print('The have have ended')
 
 if __name__ == '__main__':
-    app.run(Debug=True)
+    app.run(Debug = True)
--- models.py
+++ models.py
@@ -5,19 +5,62 @@ from scipy.stats import binom
 from math import floor
 
 class Player:
+    """
+    Player Class.
+
+    Attributes:
+        id (str): The unique identifier for every Player.
+        name (str): The name of the Player.
+        score (int): The total points that the Player has.
+        pos (list): The current position of the Player in the Board. 
+    """
+
     def __init__(self, id:int, name:str) -> None:
+        """
+        Initialize Player Class, the default score and position is 0.  
+
+        Args:
+            id (str): The unique identifier for every Player.
+            name (str): The name of the Player.
+        """
+
         self.id = id
         self.name = name
         self.score = 0
         self.pos = [0,0]
 
     def change_pos(self, move:int, board_x:int, board_y:int) -> None:
-        self.pos = [(self.pos[0] + move) % board_x, ((self.pos[0]+move) // board_y) + self.pos[1]]
+        """
+        Function that changes the attribute Position of the Player.
+
+        Args:
+            move (int)
+            board_x (int)
+            board_y (int)
+
+        Returns:
+            None
+        """
+
+        self.pos = [(self.pos[0] + move) % board_x, ((self.pos[0] + move) // board_y) + self.pos[1]]
         
     def change_score(self, change: int) -> None:
+        """
+        Function that changes the attribute Score of the Player.
+
+        Args:
+            change (int)
+
+        Returns:
+            None
+        """
+
         self.score += change
 
     def __repr__(self) -> str:
+        """
+        Create the string representation of the Player Class.
+        """
         return f'Player {self.id}: {self.name} | Position: {self.position} | Score: {self.score}'
 
 
@@ -30,7 +73,8 @@ class Board:
         return [[Cell(x, y) for y in np.arange(0, self.height)] for x in np.arange(0, self.width)]
 
 class Ladder:
-    def __init__(self, board_x:int, board_y: int) -> None:
+    def __init__(self, id:int, board_x:int, board_y: int) -> None:
+        self.id = id
         self.path = self.generate_pos(board_x, board_y)
 
     def generate_pos(self, board_x:int, board_y:int) -> dict:
@@ -41,7 +85,8 @@ class Ladder:
         return {'begin': begin, 'end': end}
     
 class Snake:
-    def __init__(self, board_x:int, board_y: int) -> None:
+    def __init__(self, id:int, board_x:int, board_y: int) -> None:
+        self.id = id
         self.path = self.generate_pos(board_x, board_y)
 
     def generate_pos(self, board_x:int, board_y:int) -> dict:
@@ -63,16 +108,13 @@ class Cell:
         self.x = x
         self.y = y
         self.has_object = False
+        self.object = None
 
 class Game:
-    def __init__(self, players:list, board:Board, dices:list, turn_limit:int, difficulty: str) -> None:
-        self.players = players
-        self.num_players = len(players)
-        
+    def __init__(self, players:list, board:Board, dices:list, difficulty: str) -> None:
+        self.players = random.shuffle(players)
         self.board = board
         self.dices = dices
-        
-        self.turn_limit = turn_limit
         self.difficulty = difficulty
         
         self.probability = {'Fácil': 0.85, 
@@ -80,24 +122,40 @@ class Game:
                             'Difícil': 0.25, 
                             'Legendaria': 0.01}
         
+        #Determine the number of Snakes & Ladders the game will have according to the difficulty
         self.n_ladders = self.get_n_ladders(self, board = self.board, threshold = 0.9)
         self.n_snakes = self.get_n_snakes(self, board = self.board, threshold = 0.9)
         
+        #Generate all the Cells of the Board
         self.cells = self.board.generate_cells()
         
         #Put a special event on the first and last cell so we don't have snakes or ladders
         self.cells[0][0].has_object = True
         self.cells[-1][-1].has_object = True
         
+        #Generate the Snakes & Ladders
         self.snakes = self.get_snake_ladders(Snake, self.n_snakes, self.board.width, self.board.height)
         self.ladders = self.get_snake_ladders(Ladder, self.n_ladders, self.board.width, self.board.height)
+
+        #Get the position of the Snakes & Ladders
+        self.snakes = [x.path for x in self.snakes]
+        self.ladders = [x.path for x in self.ladders]
+
+        #Sort the position of the Snakes & Ladders
+        self.snakes = sorted(self.snakes, key = lambda x: x['begin'])
+        self.ladders = sorted(self.ladders, key = lambda x: x['begin'])
 
     def roll_dices(self, dices:list) -> int:
         final_roll = [dices[0].get_roll(), dices[1].get_roll()]
 
-        # determine extra roll
-        if final_roll[0] == final_roll[1]: return dices[0].get_roll() + sum(final_roll)
-        else: return sum(final_roll)
+        #Determine extra roll
+        if final_roll[0] == final_roll[1]:
+            print('You Get an Extra Roll!!!')
+
+            return dices[0].get_roll() + sum(final_roll)
+        else: 
+            
+            return sum(final_roll)
 
     def get_n_snakes(self, board: Board, threshold: float = 0.9) -> int:
         n = round(threshold * ((self.board.width + self.board.height) / 2))
@@ -118,21 +176,45 @@ class Game:
         return s1 and s2
 
     def get_snake_ladders(self, Obj: Snake or Ladder, n_objs: int,  x: int, y: int) -> list:
-        obj = Obj(x, y)
+        obj = Obj(0, x, y)
         obj_pos = obj.generate_pos(x, y)
 
         objs = []
+        i = 1
 
         while len(objs) != n_objs: 
             if not self.get_bools(obj_pos):
-                obj = Obj(x, y)
+                obj = Obj(0, x, y)
                 obj_pos = obj.generate_pos(x, y)
             else:
+                obj.id = i
+
                 self.cells[obj_pos['begin'][0]][obj_pos['begin'][1]].has_object = True 
                 self.cells[obj_pos['end'][0]][obj_pos['end'][1]].has_object = True
-                objs.append(obj_pos)
+                self.cells[obj_pos['end'][0]][obj_pos['end'][1]].object = obj
 
-        return sorted(objs, key = lambda x: x['begin'])
-        
+                objs.append(obj)
 
+                i += 1
+
+        return objs
     
+    def change_player_pos_and_score(self, player: Player, move:int) -> None:
+        player.change_pos(move, self.board.x, self.board.y)
+        cell = self.cells[player.pos[0]][player.pos[1]]
+
+        if cell.has_object:
+            try:
+                new_pos = cell.object.path
+                player.pos = [new_pos['begin'], new_pos['end']]
+
+                if type(cell) == Ladder:
+                    player.change_score((move * 10) + 1)
+
+                elif type(cell) == Snake:
+                    player.change_score((move * 10) - 1)
+
+            except:
+                pass
+        else:
+            player.change_score(move * 10)
